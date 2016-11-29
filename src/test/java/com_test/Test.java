760625bcfd6747ed72372a7660bec157ba9ce228
viktor <viktor@MacBook-Air-viktor.local>
2016-11-29 21:44:25 +0200

    Added small changes

--- src/test/java/com_test/Test.java
+++ src/test/java/com_test/Test.java
@@ -1,8 +1,6 @@
 package com_test;
 
-
 import com.model.Auto;
-import com.repository.AutoRepository;
 import com.repository.AutoRepositoryImpl;
 import com.service.AutoService;
 import com.service.AutoServiceImpl;
@@ -10,15 +8,17 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(JUnit4.class)
 public class Test {
 
-    AutoService autoService;
+    private AutoService autoService;
 
     @Before
     public void makePreparationl() {
@@ -31,4 +31,11 @@ public class Test {
         Assert.assertNotNull(autos.get(0));
     }
 
+    @org.junit.Test
+    public void checGetAuto() {
+        List<Auto> audios = new ArrayList<Auto>(1);
+        audios.add(new Auto("test", 1.1, 2.2, 3.3));
+        AutoService autoService = mock(AutoService.class);
+        when(autoService.getAutos()).thenReturn((List<Auto>) audios);
+    }
 }
